3f36b16ffae1ce4bc8a84e7b1488de533c17de92
Andrin Meier <andrin@pricemeier.ch>
2022-11-19 07:30:49 +0100

    First draft of app using remix/tailwind
    Open problem: Audio doesn't work because remix runs it on server, alternatives?

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # Helpful Timer
-
-Timer app that regularly reports its progress in a respectful way.
-
-## Vision
-
-Sometimes you want a timer that doesn't just let you know when it's expired but also report its progress.
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#050103',
+      'green': '#08602b',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
